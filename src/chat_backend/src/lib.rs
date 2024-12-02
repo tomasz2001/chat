23578555a18c38fa6735bd33e021ca7f99bc2b54
basic <tomasz.m2001@gmail.com>
2024-12-02 19:52:18 +0100

    up date

--- src/chat_backend/src/lib.rs
+++ src/chat_backend/src/lib.rs
@@ -1,3 +1,22 @@
+use std::cell::RefCell;
+
+thread_local! {
+    static MSG: RefCell<String> = RefCell::new(String::new());
+
+
+}
+
+#[ic_cdk::query]
+fn get() -> String{
+    MSG.with(|msg| msg.borrow().clone())
+}
+
+#[ic_cdk::update]
+fn set(new_msg: String){
+    MSG.with(|msg| *msg.borrow_mut() = new_msg)
+}
+
+
 #[ic_cdk::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
